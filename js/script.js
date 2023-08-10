abffe01de2b31e4da9b768f924745b8bb666823e
Fábio Adaniya <f1hiroki@outlook.com>
2023-08-09 22:29:27 -0300

    ajustes
    divisão de arquivos (css e js) e melhoria no direcionamento de links

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function acessarGitHub()
+{
+    window.location.href = "https://github.com/fabio-adaniya";
+}
+
+function acessarLinkedin()
+{
+    window.location.href = "https://www.linkedin.com/in/fabio-adaniya";
+}
